35955b50c101fbc5f4a47ddef346b46a2fb10f8c
declantheedev <declanthedev@gmail.com>
2025-09-15 05:08:27 -0900

    missed an 'a'

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,7 +4,7 @@ if weather == 'sunny':
 elif weather == 'rainy':
     print("Don't forget to take an umbrella.");
 elif weather == 'cold':
-    print("Make sure to wear a warm coat and scarf.");
+    print("Make sure to wear a warm coat and a scarf.");
 else:
     print("Sorry, I don't have recommendations for this weather.")
     
