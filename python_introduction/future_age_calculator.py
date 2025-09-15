efc20b7c8c2978c26ea5c56882b1b7586ebd92ad
declantheedev <declanthedev@gmail.com>
2025-09-15 02:18:23 -0900

    feat: created a future_age_calculator file that calculates the age of a user by 2050

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+users_age = int(input("How old are you? "));
+year_born = 2023 - users_age;
+
+users_age_in_2050 = 2050 - year_born;
+print("In 2050, you will be " + str(users_age_in_2050) + " years old.");
