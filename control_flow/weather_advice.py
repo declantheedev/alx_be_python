97e895c07788af1a81d3abf2165eab113b5defd0
declantheedev <declanthedev@gmail.com>
2025-09-15 03:25:45 -0900

    created a weather_advice that gives a revommenation, based on the input of the user about the weather

--- control_flow/weather_advice.py
+++ control_flow/weather_advice.py
@@ -2,7 +2,7 @@ current_weather = input('Whats the weather like today? (sunny/rainy/cold):').str
 if current_weather == 'sunny':
     print('Wear a t-shirt and sunglasses'); 
 elif current_weather == 'rainy':
-    print("Don't forget to take an umbrella");
+    print("Don't forget to take an umbrella.");
 elif current_weather == 'cold':
     print("Make sure to wear a warm coat and scarf");
 else:
