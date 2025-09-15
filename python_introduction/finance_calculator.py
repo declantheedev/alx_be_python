13399402fcdb509c4db138794316ea720316032f
declantheedev <declanthedev@gmail.com>
2025-09-15 02:42:35 -0900

    created a file: financial_calculator that can be used to calculate user's monthly savings based on input

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+monthly_income = int(input('Enter your monthly income:'));
+total_monthly_expenses = int(input('Enter your total monthly expenses:'));
+monthly_savings = monthly_income - total_monthly_expenses;
+annual_interest = 0.05;
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * annual_interest);
+print('Your monthly savings are: {}'.format(monthly_savings));
+print('Projected savings after one year, with interest is: {}'.format(projected_savings));
