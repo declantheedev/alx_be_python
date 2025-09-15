5156345af214547a4491a526178df34212f2356a
declantheedev <declanthedev@gmail.com>
2025-09-15 03:27:55 -0900

    made some edits to the initial current_weather variable

--- control_flow/weather_advice.py
+++ control_flow/weather_advice.py
@@ -1,9 +1,9 @@
-current_weather = input('Whats the weather like today? (sunny/rainy/cold):').strip().lower();
-if current_weather == 'sunny':
+weather = input('Whats the weather like today? (sunny/rainy/cold):').strip().lower();
+if weather == 'sunny':
     print('Wear a t-shirt and sunglasses'); 
-elif current_weather == 'rainy':
+elif weather == 'rainy':
     print("Don't forget to take an umbrella.");
-elif current_weather == 'cold':
+elif weather == 'cold':
     print("Make sure to wear a warm coat and scarf");
 else:
     print("Sorry, I don't have recommendations for this weather.")
